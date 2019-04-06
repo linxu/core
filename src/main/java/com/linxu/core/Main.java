69d784ae4dfa1cd572a8668706e9f79c14a30eb6
linxu <linxu516@qq.com>
2019-04-06 16:02:11 +0800

    init

---
+++ src/main/java/com/linxu/core/Main.java
@@ -0,0 +1,11 @@
+package com.linxu.core;
+
+/**
+ * @author Alfred
+ * @date 2019-04-06
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
